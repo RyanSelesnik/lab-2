9429fa41df0eb73bd4993b94fed87e9b836b6b1c
Ryan Selesnik <selesnikryan@gmail.com>
2021-09-03 13:06:45 +0200

    Exercise 2.1

--- src/vector/size_capacity.cpp
+++ src/vector/size_capacity.cpp
@@ -1,3 +1,13 @@
+// A vector's size refers to how elements are currently stored within the vector
+// A vector's capacity refers to how many elements it is able to store before
+// having to reallocate memory.
+
+// The vector capacity grows as it does since it is a contiguous data structure.
+// i.e, if the push_back method is called continuously, eventually the memory
+// will collide with other data. To avoid this all the elements are copied to a
+// new location where the capacity has doubled (if the pervious location
+// capacity has reached its maximum).
+
 // size_capacity.cpp
 // Vector size versus capacity
 
